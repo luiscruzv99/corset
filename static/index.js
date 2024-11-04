08ec84185d48024f7c0cc9d11a11b38992b59b59
Luis C. <luiscruzv99@gmail.com>
2024-11-04 19:53:00 +0100

    Memory consumption improvements

--- static/index.js
+++ static/index.js
@@ -10,11 +10,9 @@ const ctx = canvas.getContext('2d')
 async function compress_file(file){
     if (file['type'].split('/')[0] === 'image'){
             const url = URL.createObjectURL(file)
-            const img = new Image()
+            let img = new Image()
             img.src = url;
             await img.decode()
-
-            console.log(img)
 
             canvas.width = img.naturalWidth
             canvas.height = img.naturalHeight
@@ -25,25 +23,27 @@ async function compress_file(file){
                 console.log(blob)
                 resolve(zip_file.file(`${file.name.split('.')[0]}.webp`, blob))
             }, 'image/webp', 0.8))
+            
+            img.src = ''
+            img = null
         }
 }
 
 uploader_picker.addEventListener('change', async () =>{
-    Promise.all(Array.from(uploader_picker.files).map(compress_file))
-        .then( () => {
-            console.log(1)
-            zip_file.generateAsync({
+    for (let file of Array.from(uploader_picker.files)){
+        await compress_file(file)
+    }
+
+    zip_file.generateAsync({
                 type: 'blob',
                 streamFiles: true
-            }).then(data => {
+    }).then(data => {
                 const link = document.createElement('a')
                 link.href = URL.createObjectURL(data)
                 link.download = 'compressed_imgs.zip'
                 link.click()
-            })
-        })
-
     })
+})
 
 
 
