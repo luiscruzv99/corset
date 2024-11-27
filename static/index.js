662f8779bfef58cf5ac879be12000f1efcdb5773
Luis C. <luiscruzv99@gmail.com>
2024-11-27 22:39:28 +0100

    Extracted compress logic to its own function and not a listener

--- static/index.js
+++ static/index.js
@@ -7,44 +7,50 @@ const canvas = document.createElement('canvas')
 const ctx = canvas.getContext('2d')
 
 
-async function compress_file(file){
-    if (file['type'].split('/')[0] === 'image'){
-            let url = URL.createObjectURL(file)
-            let img = new Image()
-            img.src = url;
-            await img.decode()
+async function compress_file(file) {
+    if (file['type'].split('/')[0] === 'image') {
+        let url = URL.createObjectURL(file)
+        let img = new Image()
+        img.src = url;
+        await img.decode()
 
-            canvas.width = img.naturalWidth
-            canvas.height = img.naturalHeight
-                    
-            ctx.drawImage(img, 0, 0)
+        canvas.width = img.naturalWidth
+        canvas.height = img.naturalHeight
 
-            await new Promise(resolve => canvas.toBlob((blob) => {
-                console.log(blob)
-                resolve(zip_file.file(`${file.name.split('.')[0]}.webp`, blob))
-            }, 'image/webp', 0.8))
-            
-            img.src = ''
-            img = null
-            url = null
-        }
+        ctx.drawImage(img, 0, 0)
+
+        await new Promise(resolve => canvas.toBlob((blob) => {
+            console.log(blob)
+            resolve(zip_file.file(`${file.name.split('.')[0]}.webp`, blob))
+        }, 'image/webp', 0.8))
+
+        img.src = ''
+        img = null
+        url = null
+    }
 }
 
-uploader_picker.addEventListener('change', async () =>{
-    for (let file of Array.from(uploader_picker.files)){
+async function compress() {
+
+    if (uploader_picker.files.length === 0) {
+        console.log("No files to compress")
+        return
+    }
+
+    for (let file of Array.from(uploader_picker.files)) {
         await compress_file(file)
     }
 
     zip_file.generateAsync({
-                type: 'blob',
-                streamFiles: true
+        type: 'blob',
+        streamFiles: true
     }).then(data => {
-                const link = document.createElement('a')
-                link.href = URL.createObjectURL(data)
-                link.download = 'compressed_imgs.zip'
-                link.click()
+        const link = document.createElement('a')
+        link.href = URL.createObjectURL(data)
+        link.download = 'compressed_imgs.zip'
+        link.click()
     })
-})
+}
 
 
 
